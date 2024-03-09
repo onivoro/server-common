6c095688a7016886ded0ae718b8568e77238cc1f
icedlee337 <lee.e.norris@gmail.com>
2024-03-08 19:37:17 -0600

    fix: saveFileAsJson

--- src/lib/functions/save-file-as-json.function.ts
+++ src/lib/functions/save-file-as-json.function.ts
@@ -2,7 +2,8 @@ import { writeFile } from 'fs/promises';
 
 export async function saveFileAsJson(path: string, contents: any) {
   await writeFile(
-    `${path}.json`.replace('.json.json', ''),
-    JSON.stringify(contents, null, 2)
+    `${path}.json`.replace('.json.json', '.json'),
+    JSON.stringify(contents, null, 2),
+    { encoding: 'utf-8' }
   );
 }
